81dc14df3fb81d129d264217f09bfab904785961
Pac <paco720@gmail.com>
2026-05-24 23:41:11 +0200

    Add validation attributes to delivery DTOs

--- TransportFlow.API/DTOs/DeliveryDto.cs
+++ TransportFlow.API/DTOs/DeliveryDto.cs
@@ -1,11 +1,15 @@
-﻿namespace TransportFlow.API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+namespace TransportFlow.API.DTOs
 {
     public class DeliveryDto
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
         public string CustomerName { get; set; } = string.Empty;
-
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
         public string Address { get; set; } = string.Empty;
 
         public DateTime DeliveryDate { get; set; }
